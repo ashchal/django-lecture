068d6e3134a8faf0cc2b3d5c9fa5dfc281204435
ashchalstha <ashchal93@gmail.com>
2018-06-13 09:20:14 +0545

    list

--- lists.py
+++ lists.py
@@ -1,3 +1,20 @@
-mylists=[1,2,3]
-mylist=['sdsd',1,2,3,5,True,[1,2,3]]
-print(len(mylist))
+mylists=['a','b','c','d','e']
+mylist=['a','b','c','d','e']
+
+mylists.append('new')
+print(mylists)
+
+mylist.reverse()
+print(mylist)
+
+num=[1,2,38,4,3]
+num.sort()
+print(num)
+
+newlist=[1,2,['x','y','z']]
+print(newlist[2][2])
+
+#list comprehension
+matrix=[[1,2,3],[4,5,6],[7,8,9]]
+first_col=[row[0] for row in matrix]
+print(first_col)
